b04a7e95108eb367bdd4a5ad910abd02283c7e4d
Devyash Saini <dysaini2004@gmail.com>
2026-05-29 01:38:52 +0530

    chore: remove example webhook files from git and disk

--- examples/create-checkout.ts
+++
@@ -1,9 +0,0 @@
-import { biller } from "./scrawn/biller.ts";
-
-const userId = "c0971bcb-b901-4c3e-a191-c9a97871c36f";
-
-const url = await biller.collectPayment(userId);
-
-console.log(`\nCheckout URL for ${userId}:`);
-console.log(url);
-console.log();
