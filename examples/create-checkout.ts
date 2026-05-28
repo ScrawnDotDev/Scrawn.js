1a9f2c97ac6de8387c410216df0e696eba176578
Devyash Saini <dysaini2004@gmail.com>
2026-05-29 01:37:54 +0530

    chore: remove example webhook files

--- examples/create-checkout.ts
+++ examples/create-checkout.ts
@@ -1,6 +1,6 @@
 import { biller } from "./scrawn/biller.ts";
 
-const userId = "c0971bcb-b901-4c3e-a191-c9a97871c30f";
+const userId = "c0971bcb-b901-4c3e-a191-c9a97871c36f";
 
 const url = await biller.collectPayment(userId);
 
