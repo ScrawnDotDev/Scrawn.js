06f23391e02f35d9165a3ce55bd5ab50d3263c93
Devyash Saini <dysaini2004@gmail.com>
2026-05-29 15:05:44 +0530

    Revert "feat(example): create-checkout and webhook-server examples"
    This reverts commit d5e21478debd06497ae0eb348aeb6bbed1eccdaa.

--- examples/scrawn/create-checkout.ts
+++
@@ -1,9 +0,0 @@
-import { biller } from "./biller.ts";
-
-const userId = "c0971bcb-b901-4c3e-a191-c9a97871c39f";
-
-const url = await biller.collectPayment(userId);
-
-console.log(`\nCheckout URL for ${userId}:`);
-console.log(url);
-console.log();
