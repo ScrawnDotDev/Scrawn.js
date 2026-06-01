b855795aba277e1f83acab1e395151438db8f561
Devyash Saini <dysaini2004@gmail.com>
2026-06-02 00:00:09 +0530

    fix: add httpUrl to example biller config

--- examples/scrawn/biller.ts
+++ examples/scrawn/biller.ts
@@ -4,6 +4,7 @@ import { TAGS, EXPRESSIONS } from "./pricerefs.ts";
 export const biller = scrawn({
   apiKey: process.env.SCRAWN_KEY as string,
   baseURL: process.env.SCRAWN_BASE_URL as string,
+  httpUrl: process.env.SCRAWN_HTTP_URL as string,
   secure: process.env.SCRAWN_BASE_URL?.startsWith("https") ?? false,
   tags: TAGS,
   expressions: EXPRESSIONS,
